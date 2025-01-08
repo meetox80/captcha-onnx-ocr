fddbdde389f7470326245eef06fcd153e3ab8761
lmq <72318911+meetox80@users.noreply.github.com>
2025-01-08 02:11:43 +0100

    todo: tests

---
+++ tests/config.js
@@ -0,0 +1,12 @@
+// tests/config.js
+module.exports = {
+    TEST_COUNT: 1000,
+    SAVE_FAILED: true,
+    OUTPUT_DIR: './tests/data/generated',
+    IMAGE_CONFIG: {
+        width: 128,
+        height: 32,
+        length: 6,
+        charset: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+};
